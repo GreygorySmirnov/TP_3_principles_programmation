536d5ad0a39a5adae4da8434b8798376220b4ea0
arf-hamza <hamza.arfaoui03@outlook.fr>
2023-02-07 22:59:24 -0500

    Validation formulaire et afficher message erreur

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-test commit
+let nombrePaire = 0;
+let formulaire = document.getElementById("formulaire").addEventListener("submit", function(e) {validerFormulaire(e);});
+
+
+function validerFormulaire(e){
+    let nombrePaire = 0;
+    let nom = ""
+    let prenom = 0
+    nombrePaire = document.getElementById("nombrePaire").value;
+    
+    let erreurDetectee = false;
+    if(nombrePaire > 10 || nombrePaire < 2 ){
+        document.getElementById("erreur-nombrePaire").textContent = "Vous devez choisir un nombre de paire valide.";
+        erreurDetectee = true; 
+        e.preventDefault();
+    }
+    else{
+        
+        document.getElementById("erreur-nombrePaire").textContent = "";
+
+    }
+
+    if(nom == "" || nom == NaN || nom != "^[A-Za-z0-9]"){
+        document.getElementById("erreur-nom").textContent = "Vous devez saisir un nom valide.";
+        erreurDetectee = true;
+        e.preventDefault();
+    }
+    else{
+        
+        document.getElementById("erreur-nom").textContent = "";
+
+    }
+
+    if(prenom == "" || prenom == NaN || prenom != "^[A-Za-z0-9]"){
+        document.getElementById("erreur-prenom").textContent = "Vous devez saisir un prenom valide.";
+        erreurDetectee = true;
+        e.preventDefault();
+    }
+    else{
+        
+        document.getElementById("erreur-prenom").textContent = "";
+
+}
+}
