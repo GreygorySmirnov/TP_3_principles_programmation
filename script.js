8058aecb109bf5401e00a0622b864d4d3fe2375e
Sebastien Arsenault <sebastienarsenault1999@gmail.com>
2023-02-09 16:52:10 -0500

    Première mise en commun avec tous les membres de l'équipes

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let nombrePaire = 0;
-let formulaire = document.getElementById("formulaire").addEventListener("submit", function(e) {validerFormulaire(e);});
+let formulaire = document.getElementById("formulaire")
+let jeu = document.getElementById("jeu");
+
+let carte1 = null;
+let carte2 = null;
+
+formulaire.addEventListener("submit", function(e) {validerFormulaire(e);});
+jeu.hidden = true;
 
 function validerFormulaire(e){
     e.preventDefault();
-    let nombrePaire = 0;
+    e.target == formulaire;
     let nom = document.getElementById("txtNom").value;
-    nombrePaire = document.getElementById("nombrePaire").value;
+    let nombrePaire = document.getElementById("nombrePaire").value;
 
     let regNom = /^[A-Za-z]{1,}$/;
     
@@ -30,6 +37,53 @@ function validerFormulaire(e){
 
     if(erreurDetectee == false){
         formulaire.hidden = true;
-        document.getElementById("jeu").hidden = false;
+        jeu.hidden = false;
+
+        creerJeu(nombrePaire);
     }
+}
+
+function creerJeu(nombrePaire){
+    for (let index = 0; index < nombrePaire; index++) {
+        CreerCarte(index);
+        CreerCarte(index);
+    }
+}
+
+function CreerCarte(numeroCarte){
+    const carte = document.createElement("button");
+    carte.style.width = "100px";
+    carte.style.height = "100px";
+
+    carte.setAttribute("numero-carte", numeroCarte);
+    carte.addEventListener("click", function(e) {retournerCarte(e);});
+
+    jeu.appendChild(carte);
+}
+
+function retournerCarte(e){
+    if(carte1 == null){
+        carte1 = e.target;
+        e.target.textContent = e.target.getAttribute("numero-carte");
+    }
+    else{
+        carte2 = e.target;
+        e.target.textContent = e.target.getAttribute("numero-carte");
+
+        if(carte1.getAttribute("numero-carte") == carte2.getAttribute("numero-carte")){
+            e.target.textContent = e.target.getAttribute("numero-carte");
+            carte1 = null;
+            carte2 = null;
+        }
+        else{
+            setTimeout(cacherCartes, 1000);
+        }
+    }
+}
+
+function cacherCartes(){
+    carte1.textContent = "";
+    carte2.textContent = "";
+    carte1 = null;
+    carte2 = null;
 }
